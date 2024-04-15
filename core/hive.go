03dd17e018f5d11d2ed208a845aa87c4262fb059
mateus-henrique-silva <henrique.s@gsuite.iff.edu.br>
2024-04-15 11:39:22 -0300

    subindo updated

---
+++ core/hive.go
@@ -0,0 +1,24 @@
+package core
+
+import (
+	"context"
+	"log"
+
+	"go.mod/db"
+	"go.mod/entity"
+)
+
+type HiveManager struct {
+}
+
+func HiveManagerNew() *HiveManager {
+	return &HiveManager{}
+}
+
+func (m *HiveManager) Create(ctx context.Context, data entity.Hive) error {
+	if err := db.HiveCreate(ctx, data); err != nil {
+		log.Println("m *HiveManager db.HiveCreate err: ", err)
+		return err
+	}
+	return nil
+}
